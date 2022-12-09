adfe6da35b4c6804b99362bb3fba7cd68e8cbed1
nate-sys <nathandawit555@gmail.com>
2022-12-09 15:06:28 +0300

    feat: better templating
    generated page is now derived from a minimal template that exists at the root of the directory

--- src/main.rs
+++ src/main.rs
@@ -34,22 +34,37 @@ fn App<G: Html>(cx: Scope, user_data: UserData) -> View<G> {
 }
 
 fn main() {
-    // Open from user-provided json file
+    // open user-provided json file
     let mut data_file = fs::File::open("data.json").unwrap();
 
     // Deserialize file content
     let mut user_data_json = String::new();
     data_file.read_to_string(&mut user_data_json).unwrap();
     let user_data: UserData = serde_json::from_str(&user_data_json).unwrap();
-    dbg!(&user_data);
 
     // Feed data to Sycamore
     let cloned_user_data = user_data.clone();
     let html = sycamore::render_to_string(|cx| view! {cx, App(cloned_user_data)});
-    dbg!(&html);
 
-    // Write to a file
-    let mut html_file =
+    // Open the index file
+    let mut html_file = fs::File::open("index.html").unwrap();
+    let mut index_template = String::new();
+    html_file.read_to_string(&mut index_template).unwrap();
+
+    // Create a new page from the template
+    let mut generated_page =
         fs::File::create(format!("{}_data.html", user_data.github_username)).unwrap();
-    write!(html_file, "{}", html).unwrap();
+    write!(
+        generated_page,
+        "{}",
+        index_template
+            .replace("%sycamore-body%", &html)
+            .replace("%sycamore-title%", &user_data.full_name)
+    )
+    .unwrap();
+
+    println!(
+        "Page: {}_data.html has been generated.",
+        user_data.github_username
+    );
 }
