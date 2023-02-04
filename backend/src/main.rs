d53ccd3866b6da1dd5553ba4fa54a3c35396b934
nathan dawit <nathandawit555@gmail.com>
2023-02-04 15:17:01 +0300

    chore: migrate to sqlite
    used sqlite instead of postgres

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,16 +1,20 @@
 use axum::{
     extract::{Path, State},
-    http::{StatusCode, Method},
+    http::{Method, StatusCode},
     response::IntoResponse,
     routing::{get, post},
     Json, Router,
 };
-use sqlx::postgres::{PgPool, PgPoolOptions};
+
+use sqlx::{migrate::MigrateDatabase, Sqlite, SqlitePool};
+
 use std::net::SocketAddr;
-use tower_http::cors::{CorsLayer, Any};
+use tower_http::cors::{Any, CorsLayer};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 use common::Link;
+
+const DB_URL: &str = "sqlite:weird.db";
 
 #[tokio::main]
 async fn main() {
@@ -23,24 +27,40 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let db_connection_str = format!(
-        "postgres://{}:{}@localhost:5432/{}",
-        std::env::var("POSTGRES_USER").unwrap_or_else(|_| "postgres".to_string()),
-        std::env::var("POSTGRES_PASSWORD").unwrap_or_else(|_| "pass".to_string()),
-        std::env::var("POSTGRES_DB").unwrap_or_else(|_| "db".to_string())
-    );
+    if !Sqlite::database_exists(DB_URL).await.unwrap_or(false) {
+        println!("Creating database {}", DB_URL);
+        match Sqlite::create_database(DB_URL).await {
+            Ok(_) => println!("Create db success"),
+            Err(error) => panic!("error: {}", error),
+        }
+    } else {
+        println!("Database already exists");
+    }
 
-    let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect(&db_connection_str)
-        .await
-        .expect("can't connect to database");
+    let conn = SqlitePool::connect(DB_URL).await.unwrap();
+
+    let _create_table = sqlx::query(
+        r#"
+    CREATE TABLE links (
+        url TEXT PRIMARY KEY NOT NULL,
+        title TEXT NOT NULL,
+        github_username TEXT NOT NULL
+    );
+    "#,
+    )
+    .execute(&conn)
+    .await;
 
     let app = Router::new()
         .route("/:github_username", get(get_page))
         .route("/create", post(create_page))
-    .layer(CorsLayer::new().allow_origin(Any).allow_methods([Method::GET, Method::POST]).allow_headers(Any))
-        .with_state(pool);
+        .layer(
+            CorsLayer::new()
+                .allow_origin(Any)
+                .allow_methods([Method::GET, Method::POST])
+                .allow_headers(Any),
+        )
+        .with_state(conn);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
@@ -52,24 +72,25 @@ async fn main() {
 
 async fn get_page(
     Path(github_username): Path<String>,
-    State(pool): State<PgPool>,
+    State(pool): State<SqlitePool>,
 ) -> impl IntoResponse {
-    let page = sqlx::query_as::<_, Link>("select * from links where github_username = $1")
-        .bind(github_username)
+    let page = sqlx::query_as::<_, Link>("SELECT * FROM links WHERE github_username = ?")
+        .bind(&github_username)
         .fetch_all(&pool)
         .await
         .map_err(internal_error);
+
     (StatusCode::OK, axum::Json(page.unwrap()))
 }
 
 #[axum_macros::debug_handler]
 async fn create_page(
-    State(pool): State<PgPool>,
+    State(pool): State<SqlitePool>,
     Json(links): Json<Vec<Link>>,
 ) -> impl IntoResponse {
     for link in links.iter() {
         _ = sqlx::query_as::<_, Link>(
-            "insert into links (url, title, github_username)  values ($1, $2, $3)",
+            "INSERT INTO links (url, title, github_username)  VALUES (?1, ?2, ?3)",
         )
         .bind(&link.url)
         .bind(&link.title)
@@ -78,7 +99,11 @@ async fn create_page(
         .await
         .map_err(internal_error);
     }
-    ( StatusCode::CREATED, format!("Created page for {}", links[0].github_username))
+
+    (
+        StatusCode::CREATED,
+        format!("Created page for {}", links[0].github_username),
+    )
 }
 
 fn internal_error<E>(err: E) -> (StatusCode, String)
