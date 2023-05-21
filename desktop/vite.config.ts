3b2bfb54639211faaa8f426c7f424c4f7759aa1a
Noah Too <krivahtoo@gmail.com>
2023-05-21 13:52:17 +0300

    switch to sveltekit
    change ui from plain svelte to sveltkit for easier page routing

--- desktop/vite.config.ts
+++ desktop/vite.config.ts
@@ -1,9 +1,9 @@
+import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [sveltekit()],
   // prevent vite from obscuring rust errors
   clearScreen: false,
   // Tauri expects a fixed port, fail if that port is not available
