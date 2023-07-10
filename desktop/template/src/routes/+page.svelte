cc76bc6328a6d2d4f09d3302f91c070e3f6be491
Noah Too <krivahtoo@gmail.com>
2023-07-10 11:49:39 +0300

    refactor: hardcode template fields

--- desktop/template/src/routes/+page.svelte
+++ desktop/template/src/routes/+page.svelte
@@ -1,16 +1,3 @@
-<script lang="ts">
-  import type { Link } from '../../../src/bindings/Link'
-
-  let name = 'User Name'
-  let username = 'user'
-  let photo = 'https://unavatar.io/user'
-  let title = 'Programmer'
-  let links: Array<Link> = [
-    { id: 1, url: 'https://link1.example.com', text: 'Link 1' },
-    { id: 2, url: 'https://link2.example.com', text: 'Link 2' },
-  ]
-</script>
-
 <div
   class="bg-gray-200 font-sans h-screen w-full flex flex-row justify-center items-center"
 >
@@ -20,14 +7,14 @@
     <img
       id="photo"
       class="w-32 mx-auto rounded-full mt-5 border-8 border-white"
-      src={photo}
+      src="https://unavatar.io/user"
       alt=""
     />
-    <div id="name" class="text-center mt-2 text-3xl font-medium">{name}</div>
+    <div id="name" class="text-center mt-2 text-3xl font-medium">User Name</div>
     <div id="username" class="text-center mt-2 font-light text-sm">
-      @{username}
+      @username
     </div>
-    <div id="title" class="text-center font-normal text-lg">{title}</div>
+    <div id="title" class="text-center font-normal text-lg">Programmer</div>
     <div id="about" class="px-6 text-center mt-2 font-light text-sm">
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus
@@ -47,18 +34,26 @@
       </div>
     </div>
     <div id="links" class="w-full text-center">
-      {#each links as link}
-        <div class="link">
-          <a href={link.url} target="_blank">
-            <div class="link-text">
-              {link.text}
-            </div>
-            <div class="link-url">
-              {link.url}
-            </div>
-          </a>
-        </div>
-      {/each}
+      <div class="link">
+        <a href="https://link1.example.com" target="_blank">
+          <div class="link-text">
+            Link 1
+          </div>
+          <div class="link-url">
+            https://link1.example.com
+          </div>
+        </a>
+      </div>
+      <div class="link">
+        <a href="https://link2.example.com" target="_blank">
+          <div class="link-text">
+            Link 2
+          </div>
+          <div class="link-url">
+            https://link2.example.com
+          </div>
+        </a>
+      </div>
     </div>
   </div>
 </div>
